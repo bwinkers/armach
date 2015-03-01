695033744ef294bccaed5499da40fbe3338026de
Brian Winkers <bwinkers@gmail.com>
2015-03-01 11:23:42 -0600

    Stubbed out independent modules

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 // Require the Mach server code
 var mach = require('mach')
     , settings = require('./config/settings').settings
-    , activerules = require('activerules')
+    , arconfig = require('arconfig')
+    , lten = require('arlten')
+    , auth = require('arauth')
+    , route = require('arroute')
     , nugget = require('nugget');
 
 // Create an app using the default Mach stack
 var app = mach.stack();
 
-// Use the mach request logger @todo choose a full logger
+// Use the mach request logger @todo choose a full logger as well
 app.use(mach.logger);
 
-// Use ActiveRules
-app.use(activerules, settings);
+// Use ActiveRules Config
+// This add Site/Hostname based configuration, localization and routing.
+app.use(arconfig, settings);
+
+// Use ActiveRules Localization (L10N)
+app.use(lten, settings);
+
+// Use ActiveRules authentication
+app.use(auth, settings);
+
+// Use ActiveRules Routing
+app.use(route, settings);
 
 app.get('/', function (request) {
     return nugget.root(request)
