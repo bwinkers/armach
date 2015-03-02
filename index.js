11fc8ec4125add6ac66e2b0d82eabb8bbb1567cd
Brian Winkers <bwinkers@gmail.com>
2015-03-02 01:40:11 -0600

    Cleaned things up and added many comments

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
-// Require the Mach server code
+/**
+ * This launches the ActiveRules server.
+ *
+ * License: MIT
+ *
+ * Run via the command:
+ *    node index.js
+ */
+
+/**
+ * Require the Mach server code and ActiveRules modules.
+ *
+ * Mach provides a kickass Promise driven HTTP library.
+ * ActiveRules provides a web application server designed to support many domains running from a single instance.
+ *
+ * You shouldn't change this other than adding additional middleware or removing some of the default middleware,
+ */
 var mach = require('mach')
     , settings = require('./config/settings').settings
-    , arconfig = require('arconfig')
-    , lten = require('arlten')
-    , auth = require('arauth')
-    , route = require('arroute')
-    , nugget = require('nugget');
+    , arconfig = require('arconfig') // ActiveRules config
+    , lten = require('arlten') // ActiveRules localization (l10n)
+    , auth = require('arauth') // ActiveRules authentication via Passport
+    , nugget = require('nugget');  // ActiveRules Nugget JSOn schema
 
 // Create an app using the default Mach stack
 var app = mach.stack();
@@ -13,23 +28,67 @@ var app = mach.stack();
 // Use the mach request logger @todo choose a full logger as well
 app.use(mach.logger);
 
-// Use ActiveRules Config
-// This add Site/Hostname based configuration, localization and routing.
+/**
+ * Use ActiveRules Config.
+ * This adds Site/Hostname based configuration.
+ */
 app.use(arconfig, settings);
 
-// Use ActiveRules Localization (L10N)
+/**
+ * Use ActiveRules Localization (L10N).
+ * This provides translations and site or market specific localizations.
+ */
 app.use(lten, settings);
 
-// Use ActiveRules authentication
+/**
+ * Use ActiveRules Authentication.
+ * This provides authentication and authorization using Passport.
+ */
 app.use(auth, settings);
 
-// Use ActiveRules Routing
-app.use(route, settings);
+/**
+ * Use ActiveRules Nugget Schema.
+ * This provides a base of JSON object upon which others can be built.
+ */
+//app.use(nugget, settings);
 
-app.get('/', function (request) {
-    // return 'Hello world!';
-    return nugget.root(request)
-});
+//##########################################################################//
+// !!!!!! Add your middleware BELOW here. !!!!!!
+//##########################################################################//
 
-// Start the app server
+// Replace this with your middleware(s)
+// Use any of the ActiveRules ones above as an example.
+
+//##########################################################################//
+// ^^^^^ Add your middleware ABOVE here. ^^^^^
+//##########################################################################//
+
+/**
+ * Create routes from the API elements exposed through the Nugget module.
+ */
+loadControllers(app);
+
+/**
+ * Make it Active! Active Rules!
+ */
 mach.serve(app);
+
+//==========================================================================//
+// Functions.
+// You really shouldn't have to change these for a normal install.
+//==========================================================================//
+
+/**
+ * Extract all the API elements from the Nugget JSON schema create a route for each.
+ * Each Middleware route must be explicitly enabled in a site config for it to be accessed by that site.
+ *
+ * @param app
+ */
+function loadControllers(app) {
+
+    app.get('/', function (request) {
+        // return 'Hello world!';
+        return nugget.root(request)
+    });
+
+}
